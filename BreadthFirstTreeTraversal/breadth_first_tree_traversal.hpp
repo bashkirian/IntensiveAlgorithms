c4133a75ad230d20c45490eef50386ab41944086
bashkirian <exaggeratethismotion@gmail.com>
2023-08-14 20:59:15 +0300

    fifth task

--- BreadthFirstTreeTraversal/breadth_first_tree_traversal.hpp
+++ BreadthFirstTreeTraversal/breadth_first_tree_traversal.hpp
@@ -4,6 +4,8 @@
 
 // https://leetcode.com/problems/binary-tree-level-order-traversal/
 
+using namespace std;
+
 // Definition for a binary tree node.
 struct TreeNode {
   int val;
@@ -17,5 +19,25 @@ struct TreeNode {
 
 class Solution {
 public:
-  std::vector<std::vector<int>> levelOrder(TreeNode *root) {}
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    vector<vector<int>> result(2000);
+    helper(root, result);
+    auto it = result.begin();
+    for (; it != result.end(); ++it) {
+      if ((*it).empty())
+        break;
+    }
+    return vector<vector<int>>(result.begin(), it);
+  }
+  void helper(TreeNode *root, vector<vector<int>> &result, int level = 0) {
+    if (root) {
+      result[level].push_back(root->val);
+      if (root->left) {
+        helper(root->left, result, level + 1);
+      }
+      if (root->right) {
+        helper(root->right, result, level + 1);
+      }
+    }
+  }
 };

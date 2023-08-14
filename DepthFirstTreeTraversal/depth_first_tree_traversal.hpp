d6a5e0e77ecf16fb89dfb0927c4ea6ad3f3fa1cc
bashkirian <exaggeratethismotion@gmail.com>
2023-08-14 20:19:53 +0300

    4th task

--- DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
+++ DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
@@ -14,6 +14,38 @@ struct TreeNode {
 };
 
 class Solution {
+    unordered_map<int, vector<int>> node_adj;
+    vector<int> vertices;
+    unordered_set<int> visited;
 public:
-  std::vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {}
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        dfs(root, root->left);
+        dfs(root, root->right);  
+        neighbors(target->val, k);
+        return vertices;     
+    }
+
+    void dfs(TreeNode* parent, TreeNode* child) {
+        if (parent == NULL || child == NULL) return;
+        node_adj[parent->val].push_back(child->val);
+        node_adj[child->val].push_back(parent->val);
+        dfs(child, child->left);
+        dfs(child, child->right);
+    }
+
+    void neighbors(int val, int distance) {
+        if (visited.count(val)) {
+            return;
+        }
+        visited.insert(val);
+        if (distance == 0) {
+            vertices.push_back(val);
+            return;
+        }
+        else {
+            for (auto neighbor : node_adj[val]) {
+                neighbors(neighbor, distance - 1);
+            }
+        }
+    }
 };

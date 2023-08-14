9180314a9601c234c53d6db0f30916809ec1f661
bashkirian <exaggeratethismotion@gmail.com>
2023-08-14 18:26:53 +0300

    bubble_sort_solution

--- BubbleSort/bubble_sort.hpp
+++ BubbleSort/bubble_sort.hpp
@@ -2,8 +2,26 @@
 
 #include <vector>
 
+using namespace std;
+
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+  std::vector<int> sortArray(std::vector<int> &nums) {
+        int i, j;
+        bool swapped;
+        auto n = nums.size();
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums[j], nums[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+        return nums;
+  }
 };
